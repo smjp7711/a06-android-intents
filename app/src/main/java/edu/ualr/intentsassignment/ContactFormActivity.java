7e43c6a87110c55030fdd5788bf010ed8de5d51f
smjp7711 <78757005+smjp7711@users.noreply.github.com>
2021-12-14 12:45:01 -0600

    layoutofTargetActivity
    Completed Activy target layout

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -1,9 +1,28 @@
 package edu.ualr.intentsassignment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactFormActivity extends AppCompatActivity {
     // TODO 01. Create a new layout file to define the GUI elements of the ContactFormActivity.
     // TODO 02. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_form_activity);
+    }
+
+    //Called when user taps save button
+    public void onButtonClicked(View view){
+        Intent intent = new Intent(this, ContactInfoActivity.class);
+        startActivity(intent);
+    }
+
+
     // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
 }
--- app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
@@ -1,10 +1,17 @@
 package edu.ualr.intentsassignment;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactInfoActivity extends AppCompatActivity {
     // TODO 03. Create a new layout file to define the GUI elements of the ContactInfoActivity.
     // TODO 04. Define the basic skeleton of the ContactInfoActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactinfoactivity);
+    }
     // TODO 07. Create a new method that reads the contact info coming from ContactFormActivity and use it to populate the several TextView elements in the layout.
     // TODO 08. Create a new method that invokes a Phone Dialer app, using as parameter the phone number included in the contact info received from ContactFormActivity in the previous step
     // TODO 09. Create a new method that invokes a Messages app, using as parameter the phone number included in the contact info received from ContactFormActivity in the 7th step
